5bfed8a92b03752c3a0577f7792a7b8f2169fe28
Matthew Wilson <matthew@shinythings.com>
2013-12-16 10:21:29 -0800

    More properly define pins for Arduino

--- brainwave/variants/brainwave/pins_arduino.h
+++ brainwave/variants/brainwave/pins_arduino.h
@@ -63,6 +63,9 @@ static const uint8_t A7 = 45;	// F7
 #define digitalPinToPCMSK(p)    ((((p) >= 8 && (p) <= 11) || ((p) >= 14 && (p) <= 17) || ((p) >= A8 && (p) <= A10)) ? (&PCMSK0) : ((uint8_t *)0))
 #define digitalPinToPCMSKbit(p) ( ((p) >= 8 && (p) <= 11) ? (p) - 4 : ((p) == 14 ? 3 : ((p) == 15 ? 1 : ((p) == 16 ? 2 : ((p) == 17 ? 0 : (p - A8 + 4))))))
 
+
+#define digitalPinHasPWM(p)         ((p) == 12 || (p) == 13 || (p) == 14 || (p) == 20 || (p) == 21 || (p) == 22)
+
 //	__AVR_ATmega32U4__ has an unusual mapping of pins to channels
 extern const uint8_t PROGMEM analog_pin_to_channel_PGM[];
 #define analogPinToChannel(P)  ( pgm_read_byte( analog_pin_to_channel_PGM + (P) ) )
@@ -101,70 +104,159 @@ const uint16_t PROGMEM port_to_input_PGM[] = {
   (uint16_t) &PINE,
 };
 
-
-#define PIN_DEFS				\
-  DEFPIN(PA, 0),				\
-  DEFPIN(PA, 1),				\
-  DEFPIN(PA, 2),				\
-  DEFPIN(PA, 3),				\
-  DEFPIN(PA, 4),				\
-  DEFPIN(PA, 5),				\
-  DEFPIN(PA, 6),				\
-  DEFPIN(PA, 7),				\
-  DEFPIN(PB, 0),				\
-  DEFPIN(PB, 1),				\
-  DEFPIN(PB, 2),				\
-  DEFPIN(PB, 3),				\
-  DEFPIN(PB, 4),				\
-  DEFPIN(PB, 5),				\
-  DEFPIN(PB, 6),				\
-  DEFPIN(PB, 7),				\
-  DEFPIN(PC, 0),				\
-  DEFPIN(PC, 1),				\
-  DEFPIN(PC, 2),				\
-  DEFPIN(PC, 3),				\
-  DEFPIN(PC, 4),				\
-  DEFPIN(PC, 5),				\
-  DEFPIN(PC, 6),				\
-  DEFPIN(PC, 7),				\
-  DEFPIN(PD, 0),				\
-  DEFPIN(PD, 1),				\
-  DEFPIN(PD, 2),				\
-  DEFPIN(PD, 3),				\
-  DEFPIN(PD, 4),				\
-  DEFPIN(PD, 5),				\
-  DEFPIN(PD, 6),				\
-  DEFPIN(PD, 7),				\
-  DEFPIN(PE, 0),				\
-  DEFPIN(PE, 1),				\
-  DEFPIN(PE, 2),				\
-  DEFPIN(PE, 3),				\
-  DEFPIN(PE, 4),				\
-  DEFPIN(PE, 5),				\
-  DEFPIN(PE, 6),				\
-  DEFPIN(PE, 7),
-
-
-#undef DEFPIN
-#define DEFPIN(PORT, PIN) PORT
-
-// TODO(hubbe) figure out all the pins
 const uint8_t PROGMEM digital_pin_to_port_PGM[] = {
-  PIN_DEFS
+  PA, // 0
+  PA,
+  PA,
+  PA,
+  PA,
+  PA,
+  PA,
+  PA,
+  PB, // 8
+  PB,
+  PB,
+  PB,
+  PB,
+  PB,
+  PB,
+  PB,
+  PC, // 16
+  PC,
+  PC,
+  PC,
+  PC,
+  PC,
+  PC,
+  PC,
+  PD, // 24
+  PD,
+  PD,
+  PD,
+  PD,
+  PD,
+  PD,
+  PD,
+  PE, // 32
+  PE,
+  PE,
+  PE,
+  PE,
+  PE,
+  PE,
+  PE, // 39 - PE7
+  PF, // 40 - A0 - PF0
+  PF,
+  PF,
+  PF,
+  PF,
+  PF,
+  PF,
+  PF, // 47 - A7 - PF7
 };
-
-#undef DEFPIN
-#define DEFPIN(PORT, PIN) _BV(PIN)
 
 const uint8_t PROGMEM digital_pin_to_bit_mask_PGM[] = {
-  PIN_DEFS
+  _BV(0), // PA0
+  _BV(1),
+  _BV(2),
+  _BV(3),
+  _BV(4),
+  _BV(5),
+  _BV(6),
+  _BV(7),
+  _BV(0), // PB0
+  _BV(1),
+  _BV(2),
+  _BV(3),
+  _BV(4),
+  _BV(5),
+  _BV(6),
+  _BV(7),
+  _BV(0), // PC0
+  _BV(1),
+  _BV(2),
+  _BV(3),
+  _BV(4),
+  _BV(5),
+  _BV(6),
+  _BV(7),
+  _BV(0), // PD0
+  _BV(1),
+  _BV(2),
+  _BV(3),
+  _BV(4),
+  _BV(5),
+  _BV(6),
+  _BV(7),
+  _BV(0), // PE0
+  _BV(1),
+  _BV(2),
+  _BV(3),
+  _BV(4),
+  _BV(5),
+  _BV(6),
+  _BV(7),
+  _BV(0), // PF0
+  _BV(1),
+  _BV(2),
+  _BV(3),
+  _BV(4),
+  _BV(5),
+  _BV(6),
+  _BV(7),
 };
 
-// TOOD(hubbe) figure out timer mapping
-#undef DEFPIN
-#define DEFPIN(PORT, PIN) NOT_ON_TIMER
+
+// TODO(unrepentantgeek) complete this map of PWM capable pins
 const uint8_t PROGMEM digital_pin_to_timer_PGM[] = {
-  PIN_DEFS
+  NOT_ON_TIMER, //  0 - PA0
+  NOT_ON_TIMER, //  1 - PA1
+  NOT_ON_TIMER, //  2 - PA2
+  NOT_ON_TIMER, //  3 - PA3
+  NOT_ON_TIMER, //  4 - PA4
+  NOT_ON_TIMER, //  5 - PA5
+  NOT_ON_TIMER, //  6 - PA6
+  NOT_ON_TIMER, //  7 - PA7
+  NOT_ON_TIMER, //  8 - PB0
+  NOT_ON_TIMER, //  9 - PB1
+  NOT_ON_TIMER, // 10 - PB2
+  NOT_ON_TIMER, // 11 - PB3
+  TIMER2A,      // 12 - PB4
+  TIMER1A,      // 13 - PB5
+  TIMER1B,      // 14 - PB6
+  NOT_ON_TIMER, // 15 - PB7
+  NOT_ON_TIMER, // 16 - PC0
+  NOT_ON_TIMER, // 17 - PC1
+  NOT_ON_TIMER, // 18 - PC2
+  NOT_ON_TIMER, // 19 - PC3
+  TIMER3C,      // 20 - PC4
+  TIMER3B,      // 21 - PC5
+  TIMER3A,      // 22 - PC6
+  NOT_ON_TIMER, // 23 - PC7
+  NOT_ON_TIMER, // 24 - PD0
+  NOT_ON_TIMER, // 25 - PD1
+  NOT_ON_TIMER, // 26 - PD2
+  NOT_ON_TIMER, // 27 - PD3
+  NOT_ON_TIMER, // 28 - PD4
+  NOT_ON_TIMER, // 29 - PD5
+  NOT_ON_TIMER, // 30 - PD6
+  NOT_ON_TIMER, // 31 - PD7
+  NOT_ON_TIMER, // 32 - PE0
+  NOT_ON_TIMER, // 33 - PE1
+  NOT_ON_TIMER, // 34 - PE2
+  NOT_ON_TIMER, // 35 - PE3
+  NOT_ON_TIMER, // 36 - PE4
+  NOT_ON_TIMER, // 37 - PE5
+  NOT_ON_TIMER, // 38 - PE6
+  NOT_ON_TIMER, // 39 - PE7
+  NOT_ON_TIMER, // 40 - A0 - PF0
+  NOT_ON_TIMER, // 41 - A1 - PF1
+  NOT_ON_TIMER, // 42 - A2 - PF2
+  NOT_ON_TIMER, // 43 - A3 - PF3
+  NOT_ON_TIMER, // 44 - A4 - PF4
+  NOT_ON_TIMER, // 45 - A5 - PF5
+  NOT_ON_TIMER, // 46 - A6 - PF6
+  NOT_ON_TIMER, // 47 - A7 - PF7
 };
 
 const uint8_t PROGMEM analog_pin_to_channel_PGM[12] = {
